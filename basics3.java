2365b02fb0793fdf2d605afcf27be9d0707324e4
dinazzzzz <109682997+dinazzzzz@users.noreply.github.com>
2022-07-26 22:39:29 +0530

    Add files via upload

---
+++ basics3.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class CommentExample2 {    
+public static void main(String[] args) {    
+/* Let's declare and  
+ print variable in java. */    
+  int i=10;    //iniatialize variable
+    System.out.println(i);    
+/* float j = 5.9; 
+    float k = 4.4; 
+    System.out.println( j + k ); */    
+}    
+}
