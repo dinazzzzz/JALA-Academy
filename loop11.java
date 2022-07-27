02a71a5ddd2265d44296e4841ebafc32d7375366
dinazzzzz <109682997+dinazzzzz@users.noreply.github.com>
2022-07-28 00:51:45 +0530

    Add files via upload

---
+++ loop11.java
@@ -0,0 +1,20 @@
+package com.tcc.java.programs;
+ 
+import java.util.*;
+ 
+public class OddOrEvenSwitch {
+    public static void main(String args[]) {
+        int num;
+ 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an Integer");
+        num = in.nextInt();
+        switch(num%2) {
+            case 0:
+                    System.out.println(num + " is a EVEN Number");
+                    break;
+            case 1:
+                    System.out.println(num + " is ODD Number")
+        }
+    }
+}  
